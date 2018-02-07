15f548f178bc12ca8dc4c32cc5d4ce0572a78be2
ryand88 <ryand8@hotmail.com>
2018-02-07 11:04:50 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import TopNav from './components/TopNav/TopNav';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <TopNav />
+        <p className="App-intro">
+        test
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './TopNav.css';
+
+
+export default class TopNav extends Component {
+    render() {
+        return (
+            <header className="Nav-bar">
+            Hello
+            </header>
+        );
+      }
+}
