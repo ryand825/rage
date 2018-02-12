8daa60f0b48b900501f2c225ebd53b2ad29829ed
ryand88 <ryand8@hotmail.com>
2018-02-11 18:43:52 -0700

    commit

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './TopNav.css';
 
+import rageLogo from "../../assets/RAGE CORPS LOGO-02.png";
+
 
 export default class TopNav extends Component {
     render() {
         return (
-            <header className="Nav-bar">
-            Hello
-            </header>
+            <nav className="nav-bar">
+                <div className="nav-title-wrapper">
+                    <img
+                        alt="rage logo"
+                        className="nav-rage-logo"
+                        src={rageLogo}
+                    />
+                </div>
+            </nav>
         );
-      }
+    }
 }
